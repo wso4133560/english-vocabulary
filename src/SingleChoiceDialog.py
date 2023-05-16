c0f90320a4abdc6cc176d018599079547979e01e
wso4133560 <wso4133560@gmail.com>
2023-05-16 11:01:05 +0800

    fix:完成单项选择对话框的开发

--- src/SingleChoiceDialog.py
+++ src/SingleChoiceDialog.py
@@ -1,12 +1,12 @@
 import sys
 import random
-from PyQt5.QtWidgets import QApplication, QDialog, QVBoxLayout, QLabel, QButtonGroup, QRadioButton, QPushButton
+from PyQt5.QtWidgets import QApplication, QDialog, QVBoxLayout, QLabel, QButtonGroup, QRadioButton, QPushButton, QMessageBox
 
 class SingleChoiceDialog(QDialog):
     def __init__(self, words):
         super().__init__()
         self.words = words
-        
+
         self.question_label = QLabel()
         self.option_buttons = []
         self.button_group = QButtonGroup()
@@ -15,25 +15,29 @@ class SingleChoiceDialog(QDialog):
             button = QRadioButton()
             self.option_buttons.append(button)
             self.button_group.addButton(button)
-        
+
         self.submit_button = QPushButton('Submit')
-        self.submit_button.clicked.connect(self.accept)
+        self.submit_button.clicked.connect(self.get_answer)
         layout = QVBoxLayout()
         layout.addWidget(self.question_label)
         for button in self.option_buttons:
             layout.addWidget(button)
         layout.addWidget(self.submit_button)
         self.setLayout(layout)
-        
+
         self.get_random_options()
 
     def get_answer(self):
         if self.button_group.checkedButton():
             answer = self.option_buttons.index(self.button_group.checkedButton())
+            if answer == self.answer_pos:
+                reply = QMessageBox.question(self, 'Message', '选择正确', QMessageBox.Yes)
+                self.get_random_options()
+            else:
+                reply = QMessageBox.question(self, 'Message', '选择错误', QMessageBox.Yes)
         else:
-            answer = -1
-        return answer
-    
+            reply = QMessageBox.question(self, 'Message', '请选择一个选项', QMessageBox.Yes)
+
     def get_random_options(self):
         # 获取所有key组成的列表
         keys = list(self.words.keys())
@@ -43,13 +47,15 @@ class SingleChoiceDialog(QDialog):
         random_value = self.words[random_key]
 
         values = list(self.words.values())
-        self.answer_pos = random.randint(0, 4)
+        self.answer_pos = random.randint(0, 3)
+
+        false_values = set()
         for i in range(0, 4):
             if i == self.answer_pos:
                 self.option_buttons[i].setText(random_value)
             else:
                 value = random.choice(values)
-                while value == random_value:
+                while value == random_value or value in false_values:
                     value = random.choice(values)
                 self.option_buttons[i].setText(value)
-    
+                false_values.add(value)
